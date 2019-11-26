710f33d94877823b4cfea1204b75e03f8b8a0e09
Pedro Arvellos <pedro.goncalves@watchguard.com>
2019-11-26 15:03:59 -0300

    Comment about MenuItem styles

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
+  //size can be large.
   <div className={`${size} menu-item`}>
     <div
       className='background-image'
